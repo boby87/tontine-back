c8d6216c124e7c512e3f5bd3988a5208e1f0bea9
boby87 <fokoutghislain@gmail.com>
2026-04-10 05:34:42 +0400

    first commit

---
+++ src/main/java/cm/ftg/tontine/TontineApplication.java
@@ -0,0 +1,13 @@
+package cm.ftg.tontine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TontineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TontineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cm/ftg/tontine/TontineApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.ftg.tontine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TontineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
